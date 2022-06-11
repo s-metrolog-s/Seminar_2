ee7e1aaac6b15f55d7b352de960b177f7df06f01
Sergey <metrolog_omsk@mail.ru>
2022-06-11 13:03:54 +0500

    Решили три первые задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,21 @@
-﻿// 1. Напишите программу, которая принимает
-// на вход трёхзначное число и на выходе
-// показывает последнюю цифру этого числа.
+﻿// 1. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа
 
 Random rand = new Random();
-int number = rand.Next(1, 2);
+int number = rand.Next(10, 100);
+Console.WriteLine(number);
+
+int digit1 = number/10;
+Console.WriteLine(digit1);
+int digit2 = number%10;
+Console.WriteLine(digit2);
+
+if (digit1 > digit2)
+{
+    Console.WriteLine(digit1);
+}
+else
+{
+    Console.WriteLine(digit2);
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿//  11. Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+Console.WriteLine(number);
+
+//Console.Write(number.ToString()[0]);
+//Console.Write(number.ToString()[2]);
+
+Console.Write($"{number/100}{number%10}");
