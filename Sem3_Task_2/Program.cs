c73fe9fd8bfca06fc4e1f2c2f86b7b56bfc2f696
Sergey <metrolog_omsk@mail.ru>
2022-06-12 12:58:06 +0500

    Решили задачу № 2 (Семинар № 2)

---
+++ Sem3_Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Console.WriteLine("Введите номер четверти:");
+// int number = Convert.ToInt32(Console.ReadLine());
+Random rand = new Random();
+int number = rand.Next(1,5);
+Console.WriteLine($"Подобрана {number} четверть");
+
+if (number == 1)
+{
+    Console.WriteLine("Точка лежит в пределах, где X > 0; Y > 0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Точка лежит в пределах, где X < 0; Y > 0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Точка лежит в пределах, где X < 0; Y < 0");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Точка лежит в пределах, где X > 0; Y < 0");
+}
