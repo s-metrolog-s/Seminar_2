2e1fb55d5fb19b440cab11e169440be6ce7a93be
Sergey <metrolog_omsk@mail.ru>
2022-06-12 12:57:51 +0500

    Решили задачу № 1 (Семинар № 2)

---
+++ Sem_3_Task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// 17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Random rand = new Random();
+/*
+Console.WriteLine("Введите координату x:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y:");
+int y = Convert.ToInt32(Console.ReadLine());
+*/
+
+int x = rand.Next(-10, 10);
+int y = rand.Next(-10, 10);
+
+Console.WriteLine();
+//Console.WriteLine("[" + x + "; " + y + "]"); Первый способ отражения текста с переменными
+Console.WriteLine($"[ {x}; {y} ]");
+
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("Введите ненулевые значения");
+}
+else
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("Точка лежит в 1-ой четверти");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("Точка лежит в 2-ой четверти");
+    }
+        else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("Точка лежит в 3-ой четверти");
+    }
+        else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("Точка лежит в 4-ой четверти");
+    }
+}
