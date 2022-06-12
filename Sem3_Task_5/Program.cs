fa833667d6c071c5d6b8cdf14e275462331ef4e6
Sergey <metrolog_omsk@mail.ru>
2022-06-12 13:52:27 +0500

    Решили задачу № 4 (Семинар № 2)

---
+++ Sem3_Task_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Random rand = new Random();
+int N = rand.Next(1, 10);
+int count = 1;
+Console.WriteLine($"Случайное число {N}");
+
+while (count <= N)
+{
+    Console.WriteLine($"{count} * {count} -> {Math.Pow(count, 2)}");
+    count ++;
+}
+
+Console.WriteLine();
